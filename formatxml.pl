thesofproject/avs-topology-xml

commit 1dff9d4ff3c27d077c958e6317318a4a7fc033dd

Add formatxml script
For pretty XML file indentation.

xmllint does not support custom format specification while
dotnet-format, which makes use of .editorconfig file, expects .csproj as
an input.

Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

formatxml.pl
@@ -1,0 +1,99 @@
+#!/usr/bin/env perl
+# SPDX-License-Identifier: Apache-2.0
+
+use strict;
+use warnings;
+use POSIX;
+
+my $indent_size = 2;
+my $indent_char = ' ';
+my $max_line_length = 100;
+
+sub trim {
+	my $s = shift;
+
+	$s =~ s/^\s+|\s+$//g;
+	return $s;
+}
+
+# Expects two parameters: string to analyze and stack's depth.
+sub pretty_print {
+	my $s = trim(shift(@_));
+	my $total_indent_size = $indent_size * shift(@_);
+
+	if (length($s) <= $max_line_length) {
+		print($indent_char x ($total_indent_size));
+		print("$s\n");
+		return;
+	}
+
+	# Compared to /\s+/, ' ' also removes any leading whitespace.
+	my @attrs = split(' ', $s);
+
+	$s = $indent_char x ($total_indent_size);
+	# Append '<NodeName' and update size.
+	$s .= shift(@attrs);
+	$total_indent_size = length($s);
+
+	for my $a (@attrs) {
+		if (length($s) + length($a) >= $max_line_length) {
+			print("$s\n");
+			$s = $indent_char x ($total_indent_size);
+		}
+
+		$s .= " $a";
+	}
+
+	if (length($s) > $total_indent_size) {
+		print("$s\n");
+	}
+}
+
+my ($infilename, $outfilename) = @ARGV;
+my ($INFILE, $OUTFILE);
+
+die "Input filename missing\n" if (!$infilename);
+
+open($INFILE, '<', $infilename) || die $!;
+if ($outfilename) {
+	open($OUTFILE, '>', ".$outfilename.tmp") || die $!;
+	select($OUTFILE);
+}
+
+my $current_node = "";
+my $depth = 0;
+
+while (<$INFILE>) {
+	my $line = trim($_);
+
+	next if !length($line);
+
+	if ($line =~ /^<\//) {
+		$depth--;
+		die "Unbalanded node at line $.:\n$line\n" if ($depth < 0);
+
+		pretty_print($line, $depth);
+		next;
+	}
+
+	$current_node = join(' ', $current_node, $line);
+	# If current node ends here, print it.
+	if ($current_node =~ />$/) {
+		pretty_print($current_node, $depth);
+
+		# Push stack only when '>' is on its own.
+		# TODO: mixed '<node> <!-- comment -->' not supported.
+		if ($current_node !~ /[-|\/|\?]>$/) {
+			$depth++;
+		}
+		$current_node = "";
+	}
+}
+close($INFILE);
+
+if ($outfilename) {
+	select(STDOUT);
+	close($OUTFILE);
+}
+
+rename(".$outfilename.tmp", $outfilename) || die $!;

commit 8ec7bcf52510d9173b60bd72a125577573e37599

Fix parsing inlined nodes in formatxml
A node may be provided in: <name>Value</name> fashion. Currently such
construct increases the indentation-stack depth as '</' in the middle is
not checked and the indentation gets messed up.

Fixes: 1dff9d4ff3c2 ("Add formatxml script")
Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

formatxml.pl
@@ -83,7 +83,7 @@ while (<$INFILE>) {
 
 		# Push stack only when '>' is on its own.
 		# TODO: mixed '<node> <!-- comment -->' not supported.
-		if ($current_node !~ /[-|\/|\?]>$/) {
+		if ($current_node !~ /<\/|[-|\/|\?]>$/) {
 			$depth++;
 		}
 		$current_node = "";